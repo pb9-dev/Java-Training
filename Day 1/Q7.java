469aea6f041bb3243d5b021098896a01ad6d084a
Pranav Bajaj <notpb9@gmail.com>
2024-07-30 16:24:08 +0530

    added Q7

---
+++ Day 1/Q7.java
@@ -0,0 +1,9 @@
+public class Q7 {
+    public static void main(String[] args) {
+        int stuCount = 90; 
+        int boysCount = 45;
+        double stuCountA = 0.5*stuCount;
+        double girlsCountA = stuCountA-20;
+        System.out.println("Girls getting A grade are "+(int)girlsCountA);
+    }
+}
