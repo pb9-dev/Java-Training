1e7c82808e9d0a38a561f5957c2b7fd5902942a8
Pranav Bajaj <notpb9@gmail.com>
2024-07-30 15:40:45 +0530

    Made few changes

--- Day 1/Q3.java
+++ Day 1/Q3.java
@@ -1,6 +1,6 @@
 public class Q3 {
     public static void main(String[] args) {
-        int number = 2345;
+        int n = 2345;
         int result = (((number + 8) / 3) % 5 * 5);
         System.out.println(result);
     }
