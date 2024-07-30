99c4b19979090e8232a702c77c93de1418a7e5f3
Pranav Bajaj <notpb9@gmail.com>
2024-07-30 15:54:33 +0530

    added Q5

---
+++ Day 1/Q5.java
@@ -0,0 +1,17 @@
+public class Q5 {
+    public static void main(String[] args) {
+        int a=6;
+    int b=8;
+    int c=0;
+    c=a;
+    a=b;
+    b=c;
+    System.out.println("After swapping a and b with third variable a = "+a+" b = "+b);
+    a=6;
+    b=8;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    System.out.println("After swapping a and b without third variable a= "+a+" b = "+b);
+    }
+}
