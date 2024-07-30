b0d2d67adc8f4fb52786d746a687f42f5d3a3b7d
Pranav Bajaj <notpb9@gmail.com>
2024-07-30 15:03:33 +0530

    Parimeter of traingle

---
+++ Day 1/Q2.java
@@ -0,0 +1,12 @@
+public class Q2 {
+    public static void main(String[] args) {
+
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+
+        double p = a + b + c;
+
+        System.out.println("Perimeter of triangle =" + p);
+    } 
+}
