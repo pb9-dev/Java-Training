02cf95871d37002fdf62c3a166aa8223248d14f9
Pranav Bajaj <notpb9@gmail.com>
2024-07-30 15:39:10 +0530

    Added Q4

--- Day 1/Q4.java
+++ Day 1/Q4.java
@@ -3,11 +3,11 @@ public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Subject 1 Marks ");
+        System.out.print("Enter Subject 1 Marks out of 100");
         int m1 = sc.nextInt();
-        System.out.print("Enter Subject 2 Marks ");
+        System.out.print("Enter Subject 2 Marks out of 100");
         int m2 = sc.nextInt();
-        System.out.print("Enter Subject 3 Marks ");
+        System.out.print("Enter Subject 3 Marks out of 100");
         int m3 = sc.nextInt();
 
         int t = m1 + m2 + m3;
