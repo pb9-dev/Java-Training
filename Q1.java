f113e1af3c2d02dd53c9686d3c2b27015ca1367d
Pranav Bajaj <notpb9@gmail.com>
2024-07-30 14:34:52 +0530

    Area and Parimeter of rectangle

---
+++ Q1.java
@@ -0,0 +1,14 @@
+class Q1 {
+    public static void main(String[] args) {
+       
+        double length=Double.parseDouble(args[0]);
+        double breadth=Double.parseDouble(args[1]);
+
+        double area = length * breadth;
+        double perimeter = 2*(length + breadth);
+
+        System.out.println("Area is " + area);
+        System.out.println("Perimeter is " + perimeter);
+    } 
+}
+
