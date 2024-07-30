9a69a1a5b6333b73649219fb9ee1684b90233e73
Pranav Bajaj <notpb9@gmail.com>
2024-07-30 15:44:26 +0530

    Made few changes

--- Day 1/Q4.java
+++ Day 1/Q4.java
@@ -2,19 +2,16 @@ import java.util.*;
 public class Q4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter Subject 1 Marks out of 100");
         int m1 = sc.nextInt();
         System.out.print("Enter Subject 2 Marks out of 100");
         int m2 = sc.nextInt();
         System.out.print("Enter Subject 3 Marks out of 100");
         int m3 = sc.nextInt();
-
         int t = m1 + m2 + m3;
         double p = (t / 300.0) * 100;
-
         System.out.println("Total Marks= " + t);
-        System.out.println("Percentage= " + p);
+        System.out.println("Percentage = " + p);
     }
 }
 
