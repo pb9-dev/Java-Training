38b7f7c7a0051e74c5a15ea05800c6550885fcf4
Pranav Bajaj <notpb9@gmail.com>
2024-07-31 02:49:30 +0530

    added Q10(Rverse 3 digits Number)

---
+++ Day 1/Q10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 3 digit number to reverse it");
+        int n=sc.nextInt();
+        int revNum=0;
+        while(n>0)
+        {
+            revNum = revNum*10 + n%10;
+            n=n/10;
+        }
+        System.out.println("Required sum = "+revNum);  
+        }
+}
