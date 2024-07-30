cc803dee5640a61734f954f08ae080a270aab0b9
Pranav Bajaj <notpb9@gmail.com>
2024-07-30 15:08:56 +0530

    result of (((number + 8) / 3) % 5 * 5)

--- Day 1/Q2.java
+++ Day 1/Q2.java
@@ -1,12 +1,9 @@
 public class Q2 {
     public static void main(String[] args) {
-
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
         double c = Double.parseDouble(args[2]);
-
         double p = a + b + c;
-
         System.out.println("Perimeter of triangle =" + p);
     } 
 }
